6a8170edb3456ada2d18f2542a76b42cd57ff9cc
Oliver Bonham-Carter <obonhamcarter@allegheny.edu>
2021-09-08 23:38:19 -0400

    Updating sunspot.r with code to clear the console

--- lessons/03_Intro_RProgramming/sandbox/sunspot/sunspots.r
+++ lessons/03_Intro_RProgramming/sandbox/sunspot/sunspots.r
@@ -1,6 +1,13 @@
-# date: 7 Sept 2021
+# date: 9 Sept 2021
 
 rm(list = ls()) # clear out the variables from memory to make a clean execution of the code.
+
+
+# If you want to remove all previous plots and clear the console, run the following two lines.
+graphics.off() # clear out all plots from previous work.
+
+cat("\014") # clear the console
+
 
 
 ##### Sunspot data #####
@@ -57,7 +64,7 @@ dev.off()
 
 
 # Another way to save the file.
-ggsave("ggsave_sunSpots_yearVersusSunspots_2_i.png")
+ggsave("~/Deskop/ggsave_sunSpots_yearVersusSunspots_2_i.png")
 
 
 
